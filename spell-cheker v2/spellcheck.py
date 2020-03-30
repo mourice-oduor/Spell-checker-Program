fc89b6e4027e15552bb3490f8805695994e2ed86
mourice-oduor <otienomaurice364@gmail.com>
2020-03-30 00:10:03 -0400

    spell-cheker version 2

---
+++ spell-cheker v2/spellcheck.py
@@ -0,0 +1,21 @@
+dictionary = set()
+
+def read_dictionary_file():
+    global dictionary
+    if dictionary:
+        return
+
+    with open("wordlist.txt", "r") as f:
+        contents = f.read()
+
+    dictionary = set(
+        word.lower()
+        for word in contents.splitlines()
+    )
+
+
+def is_spelled_correctly(word):
+    # Return True if spelled correctly; false otherwise
+    word = word.lower()
+    read_dictionary_file()
+    return word in dictionary
